f159cefacbdb2b0b30b248a2761f21749b2d94fe
curioussheva <jadmiko@gmail.com>
2026-04-30 11:15:18 +0700

    11

--- android/app/src/main/java/com/pristineaudio/app/MainApplication.kt
+++ android/app/src/main/java/com/pristineaudio/app/MainApplication.kt
@@ -3,55 +3,53 @@ package com.pristineaudio.app
 import android.app.Application
 import android.content.res.Configuration
 
-// CRITICAL: Pastikan import ini mengarah ke package ID yang benar!
-import com.pristineaudio.app.BuildConfig 
-
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.ReactHost
-import com.facebook.react.common.ReleaseLevel
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
 
 import expo.modules.ApplicationLifecycleDispatcher
-import expo.modules.ReactNativeHostWrapper
 import com.pristineaudio.USBDACPackage
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
-      this,
-      object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> =
-            PackageList(this).packages.apply {
-              // Add custom native modules
-              add(USBDACPackage())
-            }
+  override val reactNativeHost: ReactNativeHost =
+    object : DefaultReactNativeHost(this) {
 
-        override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
-        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-        
-        // Memastikan flag New Arch diambil dari build.gradle via BuildConfig
-        override val isNewArchEnabled: Boolean = true
-        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-      }
-  )
+      override fun getPackages(): List<ReactPackage> =
+        PackageList(this).packages.apply {
+          add(USBDACPackage())
+        }
+
+      override fun getJSMainModuleName(): String =
+        ".expo/.virtual-metro-entry"
+
+      override fun getUseDeveloperSupport(): Boolean =
+        BuildConfig.DEBUG
+
+      override val isNewArchEnabled: Boolean =
+        BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+
+      override val isHermesEnabled: Boolean =
+        BuildConfig.IS_HERMES_ENABLED
+    }
 
   override val reactHost: ReactHost
-    get() = ReactNativeHostWrapper.createReactHost(applicationContext, reactNativeHost)
+    get() = reactNativeHost.reactHost
 
   override fun onCreate() {
     super.onCreate()
 
-    // 1. Inisialisasi SoLoader dengan Merged So Mapping (Performa New Arch)
     SoLoader.init(this, OpenSourceMergedSoMapping)
 
-    // 2. Konfigurasi Entry Point New Architecture
-    DefaultNewArchitectureEntryPoint.load()
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      DefaultNewArchitectureEntryPoint.load()
+    }
 
     ApplicationLifecycleDispatcher.onApplicationCreate(this)
   }
