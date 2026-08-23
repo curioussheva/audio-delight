73c7bf6550064b9197930f424df137c60d70ad31
curioussheva <jadmiko@gmail.com>
2026-08-24 02:52:22 +0700

    mainapplication.kt

--- android/app/src/main/java/com/pristineaudio/app/MainApplication.kt
+++ android/app/src/main/java/com/pristineaudio/app/MainApplication.kt
@@ -5,45 +5,27 @@ import android.content.res.Configuration
 
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactNativeHost
-import com.facebook.react.ReactPackage
 import com.facebook.react.ReactHost
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
-import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.soloader.OpenSourceMergedSoMapping
-import com.facebook.soloader.SoLoader
+import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 
 import expo.modules.ApplicationLifecycleDispatcher
+import expo.modules.ExpoReactHostFactory
 import com.pristineaudio.USBDACPackage
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactNativeHost: ReactNativeHost =
-    object : DefaultReactNativeHost(this) {
-
-      override fun getPackages(): List<ReactPackage> =
-        PackageList(this).packages.apply {
-          add(USBDACPackage())
-        }
-
-      override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
-
-      override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-
-      override val isNewArchEnabled: Boolean = true
-
-      override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-    }
+  override val reactHost: ReactHost by lazy {
+    ExpoReactHostFactory.getDefaultReactHost(
+      context = applicationContext,
+      packageList = PackageList(this).packages.apply {
+        add(USBDACPackage())
+      }
+    )
+  }
 
   override fun onCreate() {
     super.onCreate()
-
-    SoLoader.init(this, OpenSourceMergedSoMapping)
-
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      DefaultNewArchitectureEntryPoint.load()
-    }
-
+    loadReactNative(this)
     ApplicationLifecycleDispatcher.onApplicationCreate(this)
   }
 
@@ -51,4 +33,4 @@ class MainApplication : Application(), ReactApplication {
     super.onConfigurationChanged(newConfig)
     ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig)
   }
-} 
+}
