62d30698a91eab7aa0e60442b10c0e4ed2d653a0
curioussheva <jadmiko@gmail.com>
2026-08-27 19:09:32 +0700

    bismillah

--- src/MediaStoreModule.ts
+++
@@ -1,9 +0,0 @@
-import type { TurboModule } from 'react-native';
-import { TurboModuleRegistry } from 'react-native';
-
-export interface Spec extends TurboModule {
-  queryAudioFiles(): Promise<object[]>;
-  getAlbumArtUri(albumId: string): Promise<string>;
-}
-
-export default TurboModuleRegistry.getEnforcing<Spec>('MediaStoreModule');
--- src/NativeDeviceModule.ts
+++
@@ -1,9 +0,0 @@
-import type { TurboModule } from 'react-native';
-import { TurboModuleRegistry } from 'react-native';
-
-export interface Spec extends TurboModule {
-  getDevices(): Promise<object[]>;
-  setActiveDevice(deviceId: string): Promise<boolean>;
-}
-
-export default TurboModuleRegistry.getEnforcing<Spec>('NativeDeviceModule');
