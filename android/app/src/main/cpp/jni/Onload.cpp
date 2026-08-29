34d36c8e2d2c626c29c156ea2a0eb851aa090dfa
curioussheva <jadmiko@gmail.com>
2026-08-29 18:02:57 +0700

    autolinked1

--- android/app/src/main/cpp/jni/Onload.cpp
+++ android/app/src/main/cpp/jni/Onload.cpp
@@ -15,17 +15,14 @@ namespace facebook::react {
 std::shared_ptr<TurboModule> appModulesProvider(
     const std::string& moduleName,
     const JavaTurboModule::InitParams& params) {
-  // App-specific TurboModule
   auto pristine = PristineAudioSpec_ModuleProvider(moduleName, params);
   if (pristine != nullptr) {
     return pristine;
   }
-  // Core RN specs
   auto core = FBReactNativeSpec_ModuleProvider(moduleName, params);
   if (core != nullptr) {
     return core;
   }
-  // Autolinked libraries
   return autolinking_ModuleProvider(moduleName, params);
 }
 
@@ -42,15 +39,14 @@ void appModulesRegisterProviders(
 
 } // namespace facebook::react
 
-// Entry point yang dipakai RN 0.76+ / 0.83
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* /*reserved*/) {
   return facebook::jni::initialize(vm, [] {
-    facebook::react::DefaultTurboModuleManagerDelegate::
-        cxxModuleProvider = &facebook::react::appModulesCxxProvider;
-    facebook::react::DefaultTurboModuleManagerDelegate::
-        javaModuleProvider = &facebook::react::appModulesProvider;
+    facebook::react::DefaultTurboModuleManagerDelegate::cxxModuleProvider =
+        &facebook::react::appModulesCxxProvider;
+    facebook::react::DefaultTurboModuleManagerDelegate::javaModuleProvider =
+        &facebook::react::appModulesProvider;
     facebook::react::DefaultComponentsRegistry::
         registerComponentDescriptorsFromEntryPoint =
             &facebook::react::appModulesRegisterProviders;
   });
-} 
+}
